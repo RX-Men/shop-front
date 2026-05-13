cd2aa493fce0b5676df7f50aa68c6bdea0cafec4
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2026-05-13 05:18:35 +0300

    feat: create repository infrastructure

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'scss/dollar-variable-empty-line-before': null,
+    'custom-property-empty-line-before': null,
+  },
+};
