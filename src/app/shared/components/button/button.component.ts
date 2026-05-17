50c8dc33f0579169e098259e4aba9c5ac2f7d06e
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2026-05-18 02:41:09 +0300

    feat: create button component

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,44 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+import { IconComponent } from '../icon';
+
+import { APP_TEST_IDS } from '../../../app.test-ids';
+
+import type {
+  ActionColor,
+  ActionIconPlacement,
+  ActionSize,
+  ActionVariant,
+  ButtonType,
+} from '../../models/ui.model';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-button',
+  imports: [IconComponent],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+  host: {
+    '(click)': '_onAction($event)',
+    '(keydown)': '_onAction($event)',
+  },
+})
+export class ButtonComponent {
+  readonly type = input<ButtonType>('button');
+  readonly variant = input<ActionVariant>('contained');
+  readonly size = input<ActionSize>('m');
+  readonly color = input<ActionColor>('brand');
+  readonly icon = input<ReturnType<IconComponent['name']>>();
+  readonly iconPlacement = input<ActionIconPlacement>('end');
+  readonly loading = input<boolean>();
+  readonly disabled = input<boolean>();
+
+  protected readonly _testIds = APP_TEST_IDS.button;
+
+  protected readonly _onAction = (event: Event): void => {
+    if (this.loading() || this.disabled()) {
+      event.stopPropagation();
+      return;
+    }
+  };
+}
