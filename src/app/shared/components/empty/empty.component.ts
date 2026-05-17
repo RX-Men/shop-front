771e5a9e5feace3c4ad8082de67fbb853d6cda9c
Denis <den987655@gmail.com>
2026-05-17 15:59:12 +0300

    refactor: use icon component in empty state

--- src/app/shared/components/empty/empty.component.ts
+++ src/app/shared/components/empty/empty.component.ts
@@ -1,15 +1,14 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { IconComponent } from '../icon';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-empty',
-  imports: [NgOptimizedImage],
+  imports: [IconComponent],
   templateUrl: './empty.component.html',
   styleUrl: './empty.component.scss',
 })
 export class EmptyComponent {
   readonly title = input<string>('Data Not Found');
-  readonly imgUi = input<string>('/@/assets/icons/smile.svg');
   readonly description = input<string>('');
 }
