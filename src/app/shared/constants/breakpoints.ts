1cb49ee6238c6be793e31395007a04267e1e60c7
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2026-05-23 22:44:02 +0300

    style: update global styles

---
+++ src/app/shared/constants/breakpoints.ts
@@ -0,0 +1,9 @@
+const BREAKPOINTS = {
+  xxl: '(max-width: 1399px)',
+  xl: '(max-width: 1199px)',
+  l: '(max-width: 991px)',
+  m: '(max-width: 767px)',
+  s: '(max-width: 575px)',
+} as const;
+
+export { BREAKPOINTS };
