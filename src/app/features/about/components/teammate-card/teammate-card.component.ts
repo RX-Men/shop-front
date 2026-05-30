8aa65da27636c4a216ce5c678111dc65996f749d
Denis <den987655@gmail.com>
2026-05-30 16:47:18 +0300

    feat: add teammate-card component

---
+++ src/app/features/about/components/teammate-card/teammate-card.component.ts
@@ -0,0 +1,18 @@
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-teammate-card',
+  imports: [NgOptimizedImage],
+  templateUrl: './teammate-card.component.html',
+  styleUrl: './teammate-card.component.scss',
+})
+export class TeammateCardComponent {
+  readonly name = input.required<string>();
+  readonly role = input.required<string>();
+  readonly description = input.required<string>();
+  readonly promoCode = input.required<string>();
+  readonly imgSrc = input.required<string>();
+  readonly linkHref = input.required<string>();
+}
