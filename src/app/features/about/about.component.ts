6fc05498bba3bfe95042328f9c3c57795f6fb242
Denis <den987655@gmail.com>
2026-05-31 21:57:23 +0300

    fix: fixed about list

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -10,21 +10,33 @@ export class AboutComponent {
   readonly cards = [
     {
       id: 1,
-      image: '/assets/images/image.png',
-      name: 'Dana Young Coston',
+      name: 'Boris',
+      description:
+        'I am a serene presence driven by hidden, unseen energies. My everyday superpower is flexibility: whether I am centering divs with the magic of flexbox or doing mind-bending yoga stretches like the splits. Life is a game of chance, and just like Gambit, I’m here to bend reality, build and play it with style and grace.',
+      promoCode: 'MYSTICFLEX',
+      imgSrc: '/assets/images/Boris.jpg',
       company: 'Blondie',
+      linkHref: 'https://github.com/elrouss',
     },
     {
       id: 2,
-      image: '/assets/images/image.png',
-      name: 'Maritsa Patrinos',
-      company: 'Six Chix',
+      name: 'Denis',
+      description:
+        'I am an engineering genius encased in high-tech armor and powered by an unstoppable arc reactor. My everyday superpower is precision and automation: whether I am structuring flawless layouts with clean code or instantly solving complex technical bugs. Life is a continuous upgrade, and I am here to engineer reality, defend my innovations, and win with peak efficiency and a touch of billionaire sarcasm.',
+      promoCode: 'MYSTICFLEX',
+      imgSrc: '/assets/images/Denis.jpg',
+      company: 'Blondie',
+      linkHref: 'https://github.com/den987655',
     },
     {
       id: 3,
-      image: '/assets/images/image.png',
-      name: 'Maritsa Patrinos',
-      company: 'Six Chix',
+      name: 'Boris',
+      description:
+        'I am a serene presence driven by hidden, unseen energies. My everyday superpower is flexibility: whether I am centering divs with the magic of flexbox or doing mind-bending yoga stretches like the splits. Life is a game of chance, and just like Gambit, I’m here to bend reality, build and play it with style and grace.',
+      promoCode: 'MYSTICFLEX',
+      imgSrc: '/assets/images/Boris.jpg',
+      company: 'Blondie',
+      linkHref: 'https://github.com/elrouss',
     },
   ];
 }
