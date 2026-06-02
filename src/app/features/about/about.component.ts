afbe0f2d0d1382e164d18b2572c4b35e45daf8dd
Denis <den987655@gmail.com>
2026-06-02 22:51:15 +0300

    feat: about pages with routes.ts

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,3 +1,5 @@
+import { ABOUT_CARDS } from '@/app/features/about/about.constants';
+import { TeammateCardComponent } from '@/app/features/about/components/teammate-card/teammate-card.component';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
@@ -5,38 +7,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
+  imports: [TeammateCardComponent],
 })
 export class AboutComponent {
-  readonly cards = [
-    {
-      id: 1,
-      name: 'Boris',
-      description:
-        'I am a serene presence driven by hidden, unseen energies. My everyday superpower is flexibility: whether I am centering divs with the magic of flexbox or doing mind-bending yoga stretches like the splits. Life is a game of chance, and just like Gambit, I’m here to bend reality, build and play it with style and grace.',
-      promoCode: 'MYSTICFLEX',
-      imgSrc: '/assets/images/Boris.jpg',
-      company: 'Blondie',
-      linkHref: 'https://github.com/elrouss',
-    },
-    {
-      id: 2,
-      name: 'Denis',
-      description:
-        'I am an engineering genius encased in high-tech armor and powered by an unstoppable arc reactor. My everyday superpower is precision and automation: whether I am structuring flawless layouts with clean code or instantly solving complex technical bugs. Life is a continuous upgrade, and I am here to engineer reality, defend my innovations, and win with peak efficiency and a touch of billionaire sarcasm.',
-      promoCode: 'MYSTICFLEX',
-      imgSrc: '/assets/images/Denis.jpg',
-      company: 'Blondie',
-      linkHref: 'https://github.com/den987655',
-    },
-    {
-      id: 3,
-      name: 'Boris',
-      description:
-        'I am a serene presence driven by hidden, unseen energies. My everyday superpower is flexibility: whether I am centering divs with the magic of flexbox or doing mind-bending yoga stretches like the splits. Life is a game of chance, and just like Gambit, I’m here to bend reality, build and play it with style and grace.',
-      promoCode: 'MYSTICFLEX',
-      imgSrc: '/assets/images/Boris.jpg',
-      company: 'Blondie',
-      linkHref: 'https://github.com/elrouss',
-    },
-  ];
+  readonly cards = ABOUT_CARDS;
 }
---
+++ src/app/features/about/about.constants.ts
@@ -0,0 +1,32 @@
+export const ABOUT_CARDS = [
+  {
+    id: 1,
+    name: 'Boris Zashliapin',
+    description:
+      'I am a serene presence driven by hidden, unseen energies. My everyday superpower is flexibility: whether I am centering divs with the magic of flexbox or doing mind-bending yoga stretches like the splits. Life is a game of chance, and just like Gambit, I’m here to bend reality, build and play it with style and grace.',
+    role: 'Team Lead',
+    promoCode: 'MYSTICFLEX',
+    imgSrc: '/assets/images/Boris.jpg',
+    linkHref: 'https://github.com/elrouss',
+  },
+  {
+    id: 2,
+    name: 'Denis Semenov',
+    description:
+      'I am an engineering genius encased in high-tech armor and powered by an unstoppable arc reactor. My everyday superpower is precision and automation: whether I am structuring flawless layouts with clean code or instantly solving complex technical bugs. Life is a continuous upgrade, and I am here to engineer reality, defend my innovations, and win with peak efficiency and a touch of billionaire sarcasm.',
+    role: 'Frontend Developer',
+    promoCode: 'JARVIS',
+    imgSrc: '/assets/images/Denis.jpg',
+    linkHref: 'https://github.com/den987655',
+  },
+  {
+    id: 1,
+    name: 'Boris Zashliapin',
+    description:
+      'I am a serene presence driven by hidden, unseen energies. My everyday superpower is flexibility: whether I am centering divs with the magic of flexbox or doing mind-bending yoga stretches like the splits. Life is a game of chance, and just like Gambit, I’m here to bend reality, build and play it with style and grace.',
+    role: 'Team Lead',
+    promoCode: 'MYSTICFLEX',
+    imgSrc: '/assets/images/Boris.jpg',
+    linkHref: 'https://github.com/elrouss',
+  },
+];
