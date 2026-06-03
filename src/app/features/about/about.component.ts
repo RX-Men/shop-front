f995ecea58583f76bdf5cabef2273ef791174cf9
Denis <den987655@gmail.com>
2026-06-03 17:39:33 +0300

    feat: about pages with banner and description, add router-link

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,13 +1,14 @@
 import { ABOUT_CARDS } from '@/app/features/about/about.constants';
 import { TeammateCardComponent } from '@/app/features/about/components/teammate-card/teammate-card.component';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
-  imports: [TeammateCardComponent],
+  imports: [TeammateCardComponent, RouterLink],
 })
 export class AboutComponent {
   readonly cards = ABOUT_CARDS;
