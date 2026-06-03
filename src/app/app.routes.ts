cea34ac79c12f8e535945425ad1e44c89c75fca9
Denis <den987655@gmail.com>
2026-06-03 21:49:46 +0300

    feat: add not-found pages witch routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,9 @@ export const routes: Routes = [
     path: ROUTES.uiKit,
     loadComponent: () => import('./features/ui-kit/ui-kit.component').then((m) => m.UiKitComponent),
   },
+  {
+    path: ROUTES.notFound,
+    loadComponent: () =>
+      import('./features/not-found/not-found.component').then((m) => m.NotFoundComponent),
+  },
 ];
