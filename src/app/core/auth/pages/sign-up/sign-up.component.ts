c26868df53238daad04d47406393b15a64a75c37
prokhorovd <github@prokhorov.dev>
2026-06-03 22:44:10 +0700

    feat: create sign up page layout

--- src/app/core/auth/pages/sign-up/sign-up.component.ts
+++ src/app/core/auth/pages/sign-up/sign-up.component.ts
@@ -1,7 +1,11 @@
+import { ButtonComponent } from '@/app/shared/components/button';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePickerComponent } from '@/app/shared/components/date-picker';
+import { InputComponent } from '@/app/shared/components/input';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ButtonComponent, DatePickerComponent, InputComponent],
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
