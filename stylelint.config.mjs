f6db82c6fa5e824aa165fa0a19f6af6157edda5d
Denis <den987655@gmail.com>
2026-06-03 17:38:32 +0300

    feat: about pages with banner and description

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -10,5 +10,7 @@ export default {
         ignorePseudoElements: ['ng-deep'],
       },
     ],
+    'selector-class-pattern':
+      '^[a-z][a-z0-9]*(-[a-z0-9]+)*(__[a-z0-9]+(-[a-z0-9]+)*)?(--[a-z0-9]+(-[a-z0-9]+)*)?$',
   },
 };
