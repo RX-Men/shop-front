97823dbc1d23434702271dc0d4c5dbf6b88f6753
Denis <den987655@gmail.com>
2026-06-03 21:50:44 +0300

    feat: add not-found pages witch routes.ts

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-not-found',
-  imports: [],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
