5684b021ebb49652a42e5c9776bf08888c16bad1
Denis <den987655@gmail.com>
2026-06-04 15:22:32 +0300

    fix: fixed pages witch AboutComponent

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -2,6 +2,7 @@ import { ABOUT_CARDS } from '@/app/features/about/about.constants';
 import { TeammateCardComponent } from '@/app/features/about/components/teammate-card/teammate-card.component';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import aboutData from './about.json' with { type: 'json' };
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -12,4 +13,6 @@ import { RouterLink } from '@angular/router';
 })
 export class AboutComponent {
   readonly cards = ABOUT_CARDS;
+  readonly currentYear: number = new Date().getFullYear();
+  readonly info = aboutData;
 }
