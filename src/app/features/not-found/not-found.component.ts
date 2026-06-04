5fc70589f576fee7c42dd23d41e719b2ce905f36
Denis <den987655@gmail.com>
2026-06-04 17:04:51 +0300

    fix: fixed not-found.component

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
+  imports: [RouterLink],
 })
 export class NotFoundComponent {}
