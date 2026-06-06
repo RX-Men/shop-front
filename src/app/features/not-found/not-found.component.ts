f0a84c0bd145b1db65eb4ed198d88520da081c22
Denis <den987655@gmail.com>
2026-06-06 16:17:53 +0300

    chore: router link import

--- src/app/features/not-found/not-found.component.ts
+++ src/app/features/not-found/not-found.component.ts
@@ -1,12 +1,11 @@
 import { RouterLinkComponent } from '@/app/shared/components/router-link';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
-  imports: [RouterLink, RouterLinkComponent],
+  imports: [RouterLinkComponent],
 })
 export class NotFoundComponent {}
