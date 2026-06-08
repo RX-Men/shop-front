aeeff3de687cb601bcff32e481e7cfe000517866
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2026-06-08 22:32:08 +0300

    fix: change sign up position inside routes object for correct render by url

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,12 @@ export const routes: Routes = [
     loadComponent: () => import('./features/home/home.component').then((m) => m.HomeComponent),
   },
   {
+    path: ROUTES.signUp,
+    loadComponent: () =>
+      import('./core/auth/sign-up/sign-up.component').then((m) => m.SignUpComponent),
+    canDeactivate: [unsavedChangesGuard],
+  },
+  {
     path: ROUTES.uiKit,
     loadComponent: () => import('./features/ui-kit/ui-kit.component').then((m) => m.UiKitComponent),
   },
@@ -16,11 +22,5 @@ export const routes: Routes = [
     path: ROUTES.notFound,
     loadComponent: () =>
       import('./features/not-found/not-found.component').then((m) => m.NotFoundComponent),
-  },
-  {
-    path: ROUTES.signUp,
-    loadComponent: () =>
-      import('./core/auth/sign-up/sign-up.component').then((m) => m.SignUpComponent),
-    canDeactivate: [unsavedChangesGuard],
   },
 ];
