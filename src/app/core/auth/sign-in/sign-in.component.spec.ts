2e46526b88151c4fadaf99c9b94a5c92da37076a
prokhorovd <github@prokhorov.dev>
2026-06-21 20:17:24 +0700

    feat: #124 sign-in page layout and skeleton structure

--- src/app/core/auth/sign-in/sign-in.component.spec.ts
+++ src/app/core/auth/sign-in/sign-in.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { SignInComponent } from './sign-in.component';
 
@@ -9,6 +10,7 @@ describe('SignInComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SignInComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignInComponent);
--- src/app/core/auth/sign-in/sign-in.component.ts
+++ src/app/core/auth/sign-in/sign-in.component.ts
@@ -1,9 +1,18 @@
+import { ButtonComponent } from '@/app/shared/components/button';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { InputComponent } from '@/app/shared/components/input';
+import { RouterLinkComponent } from '@/app/shared/components/router-link';
+import { ROUTES } from '@/app/core/constants/routes';
+import signInContent from '@/app/content/pages/sign-in/sign-in.json' with { type: 'json' };
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ButtonComponent, InputComponent, RouterLinkComponent],
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
-export class SignInComponent {}
+export class SignInComponent {
+  readonly content = signInContent;
+  protected readonly _routes = ROUTES;
+}
