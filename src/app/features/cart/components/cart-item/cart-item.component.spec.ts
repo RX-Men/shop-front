56ef408d66567d66a32623761c0b73ab21164866
Denis <den987655@gmail.com>
2026-06-23 19:20:17 +0300

    fix: fixed test cart-item

--- src/app/features/cart/components/cart-item/cart-item.component.spec.ts
+++ src/app/features/cart/components/cart-item/cart-item.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import type { LineItem } from '@commercetools/platform-sdk';
 
 import { CartItemComponent } from './cart-item.component';
 
@@ -12,7 +13,25 @@ describe('CartItemComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(CartItemComponent);
+
+    fixture.componentRef.setInput('item', {
+      id: '1',
+      quantity: 1,
+      name: {
+        'en-US': 'Batman',
+      },
+      variant: {
+        sku: 'batman-1',
+        images: [],
+      },
+      totalPrice: {
+        centAmount: 499,
+      },
+    } as unknown as LineItem);
+
     component = fixture.componentInstance;
+
+    fixture.detectChanges();
     await fixture.whenStable();
   });
 
