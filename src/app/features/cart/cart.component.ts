5fd22af529785d6a7d1f27a61e0a0aafd377757f
Denis <den987655@gmail.com>
2026-06-23 17:30:57 +0300

    feat: implement cart-item.component

--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -1,23 +1,15 @@
 import { CartService } from '@/app/core/services/cart.service';
+import { CartItemComponent } from '@/app/features/cart/components/cart-item/cart-item.component';
 import { ButtonComponent } from '@/app/shared/components/button';
 import { EmptyComponent } from '@/app/shared/components/empty';
-import { IconButtonComponent } from '@/app/shared/components/icon-button';
 import { RouterLinkComponent } from '@/app/shared/components/router-link';
 import { PricePipe } from '@/app/shared/pipes/price';
-import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-cart',
-  imports: [
-    IconButtonComponent,
-    PricePipe,
-    ButtonComponent,
-    EmptyComponent,
-    RouterLinkComponent,
-    NgOptimizedImage,
-  ],
+  imports: [PricePipe, ButtonComponent, EmptyComponent, RouterLinkComponent, CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
