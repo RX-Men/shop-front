bcddb28a44676820609ced579e9b2797d1c14d78
Denis <den987655@gmail.com>
2026-07-04 23:42:55 +0300

    fix: fixed tests

--- src/app/core/auth/sign-in/sign-in.component.spec.ts
+++ src/app/core/auth/sign-in/sign-in.component.spec.ts
@@ -1,16 +1,29 @@
+import { AuthService } from '@/app/core/services/auth.service';
+import { signal } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
-
 import { SignInComponent } from './sign-in.component';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
+  const authServiceMock = {
+    login: vi.fn(),
+    register: vi.fn(),
+    isAuthenticated: signal(false),
+    customer: signal(null),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SignInComponent],
-      providers: [provideRouter([])],
+      providers: [
+        provideRouter([]),
+        {
+          provide: AuthService,
+          useValue: authServiceMock,
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SignInComponent);
