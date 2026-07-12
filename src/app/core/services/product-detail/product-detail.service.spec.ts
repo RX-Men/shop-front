2343922f239477de9731e39a075a2f8c661d1fd4
Denis <den987655@gmail.com>
2026-07-12 16:16:01 +0300

    fix: mock CommercetoolsService in product-detail service spec to prevent unhandled API errors

--- src/app/core/services/product-detail/product-detail.service.spec.ts
+++ src/app/core/services/product-detail/product-detail.service.spec.ts
@@ -1,12 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
+import { CommercetoolsService } from '../commercetools';
+import { COMMERCETOOLS_CONFIG, CommercetoolsConfig } from '../commercetools/commercetools.config';
+
 import { ProductDetailService } from './product-detail.service';
+
+const mockCommercetoolsService = { apiRoot: {} };
+
+const mockConfig: CommercetoolsConfig = {
+  projectKey: 'test-project',
+  clientId: 'test-client-id',
+  clientSecret: 'test-client-secret',
+  authUrl: 'https://auth.test.com',
+  apiUrl: 'https://api.test.com',
+  scopes: ['manage_project:test-project'],
+};
 
 describe('ProductDetailService', () => {
   let service: ProductDetailService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: CommercetoolsService, useValue: mockCommercetoolsService },
+        { provide: COMMERCETOOLS_CONFIG, useValue: mockConfig },
+      ],
+    });
     service = TestBed.inject(ProductDetailService);
   });
 
